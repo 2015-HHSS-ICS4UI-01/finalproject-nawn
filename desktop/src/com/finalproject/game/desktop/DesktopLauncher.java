0540ccc1735f4ebc88a29c02c462ce7315c31491
alimu <alimu@Ali-PC>
2016-01-05 10:41:52 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/finalproject/game/ZombiesMain.java

--- desktop/src/com/finalproject/game/desktop/DesktopLauncher.java
+++ desktop/src/com/finalproject/game/desktop/DesktopLauncher.java
@@ -7,6 +7,6 @@ import com.finalproject.game.ZombiesMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ZombiesMain(), config);
+		
 	}
 }
