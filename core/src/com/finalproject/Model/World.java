0d5dfb8b97eb900abdcca2e0c735884d836d611c
Walter <Walter@172.21.4.75>
2016-01-07 10:56:28 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/finalproject/MainGame/MainGame.java

--- core/src/com/finalproject/Model/World.java
+++ core/src/com/finalproject/Model/World.java
@@ -24,16 +24,17 @@ public class World {
         player = new Player(16,16,16,32);
         // blocks along the floor
         for(int i = 0; i < 50; i++){
+            Block a = new Block(0, i*16,16,16);
             Block b = new Block(i*16,0,16,16);
+            Block c = new Block(784,i*16,16,16);
+            Block d = new Block(i*16,784,16,16);
+            blocks.add(a);
             blocks.add(b);
+            blocks.add(c);
+            blocks.add(d);
         }
         
-        blocks.add(new Block(48,16,16,16));
-        blocks.add(new Block(96,32,16,16));
-        blocks.add(new Block(112,32,16,16));
-        blocks.add(new Block(128,96,16,16));
-        blocks.add(new Block(112,96,16,16));
-        //blocks.add(new Block(200,200,16,16));
+      
     }
     
     public void update(float delta){
