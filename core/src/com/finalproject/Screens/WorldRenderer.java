658b06a24e54ca1fd808a7402f9924cb0f72857e
nitin <nitin@Nitin-PC>
2016-01-20 11:21:58 -0500

    map finished

--- core/src/com/finalproject/Screens/WorldRenderer.java
+++ core/src/com/finalproject/Screens/WorldRenderer.java
@@ -13,7 +13,13 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.finalproject.Model.Block;
@@ -24,7 +30,6 @@ import com.finalproject.Model.Zombie;
 import static com.finalproject.Screens.AssetManager.cross;
 import java.util.ArrayList;
 
-
 /**
  *
  * @author vonhn
@@ -34,85 +39,132 @@ public class WorldRenderer {
     // my games virtual width and height
     public final int V_WIDTH = 800;
     public final int V_HEIGHT = 600;
-    
 
     private World world;
     private Player player;
     private ArrayList<Zombie> zombie;
-   // private Bullet bullet;
-    
+    // private Bullet bullet;
+
     private Viewport viewport;
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
+    
     private int mouseX;
     private int mouseY;
-    private int health;
     
+    private int health;
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private Array<Rectangle> collisionBlocks;
+    private int levelWidth;
+
     public WorldRenderer(World w) {
         world = w;
         player = world.getPlayer();
-        zombie= world.getZombie();
-        
+        zombie = world.getZombie();
+
         health = world.getPlayer().getHealth();
-        
-      //  bullet = world.getBullet();
+
+        //  bullet = world.getBullet();
         camera = new OrthographicCamera();
         viewport = new FitViewport(V_WIDTH, V_HEIGHT, camera);
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
+        
         // move the x position of the camera
-        
-        camera.position.x =  V_WIDTH / 2f;
+        camera.position.x = V_WIDTH / 2f;
         // move the y position of the camera
-        camera.position.y =  V_HEIGHT/ 2f;
-        
-        
+        camera.position.y = V_HEIGHT / 2f;
+
         // update the camera
         camera.update();
 
         // loads in the images
         AssetManager.load();
+
+        Gdx.graphics.setCursor(Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("Cursor.png")), 0, 0));
+    
+                //map
+        map = new TmxMapLoader().load("zombieMap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1f, batch);
+        renderer.setView(camera);
+
+        // create the collision rectangles for our map based on the map layer
+        collisionBlocks = new Array<Rectangle>();
+        // get collision layer
+        TiledMapTileLayer solidBlocks = (TiledMapTileLayer)map.getLayers().get("walls");
         
-        Gdx.graphics.setCursor(Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("Cursor.png")), 0,0));
+        // get the dimensions of the map
+        int mapWidth = map.getProperties().get("width", Integer.class);
+        int mapHeight = map.getProperties().get("height", Integer.class);
+        int tileWidth = map.getProperties().get("tilewidth", Integer.class);
+        int tileHeight = map.getProperties().get("tileheight", Integer.class);
+        
+        levelWidth = mapWidth;
+        
+        // loop through all of the cells, find a tile and make a rectangle
+        for (int x = 0; x < mapWidth ; x++) {
+            for(int y = 0; y < mapHeight; y++){
+                if(solidBlocks.getCell(x, y) != null){
+                    Rectangle r = new Rectangle(x, y, tileWidth, tileHeight);
+                    System.out.println("x: " + r.x + "   y: " + r.y + "   tw: " + tileWidth + "   th: " + tileHeight);
+                    collisionBlocks.add(r);
+                }
+            }    
+        }
+
+    
     }
 
     public void render(float delta) {
+        
+        // get the dimensions of the map
+        int mapWidth = map.getProperties().get("width", Integer.class);
+        int mapHeight = map.getProperties().get("height", Integer.class);
+
         // clear the screen with black
         Gdx.gl20.glClearColor(0, 0, 0, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // update the camera
-//        camera.position.x = Math.max(world.getPlayer().getX(), V_WIDTH / 2);
-//        camera.position.y = Math.max(world.getPlayer().getY(), V_HEIGHT / 2);
+        //camera.position.x = Math.max(world.getPlayer().getX(), V_WIDTH / 2);
+        //camera.position.y = Math.max(world.getPlayer().getY(), V_HEIGHT / 2);
+        camera.position.x = Math.max(world.getPlayer().getX(), mapWidth/ 2);
+        camera.position.y = Math.max(world.getPlayer().getY(), mapHeight / 2);
+
         camera.update();
-        
+
         shapeRenderer.setProjectionMatrix(camera.combined);
+        
         // links the renderer to the camera
         batch.setProjectionMatrix(camera.combined);
         
-        // tells the renderer this is the list to draw
-        batch.begin();
-         
+        // render the tile map
+        renderer.setView(camera);
+        renderer.render();
+
         //initialize the starting state of the player
         player.setState(Player.State.STANDING);
         
+        // tells the renderer this is the list to draw
+        batch.begin();
+        
         //crosshair
-        
-        
         mouseX = (int) this.getMousePosInGameWorldx();
         mouseY = (int) this.getMousePosInGameWorldy();
         batch.draw(cross, mouseX, mouseY);
-        
+
         //health bar
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.RED);
         shapeRenderer.rect(0, 580, 800, 20);
         shapeRenderer.setColor(Color.GREEN);
-        if(player.getHealth() != 0){
-        shapeRenderer.rect(0, 580, player.getHealth(), 20);
-        }      
-     
+        if (player.getHealth() != 0) {
+            shapeRenderer.rect(0, 580, player.getHealth(), 20);
+        }
+
         //if the player is standing
         if (player.getState() == Player.State.STANDING) {
             //check if hes facing south
@@ -131,7 +183,7 @@ public class WorldRenderer {
                 //output his standing west picture
                 batch.draw(AssetManager.playerStandW, player.getX(), player.getY());
             }
-            
+
         } //check if hes running
         else if (player.getState() == Player.State.RUNNING) {
             //check if hes facing south
@@ -168,60 +220,61 @@ public class WorldRenderer {
                 batch.draw(AssetManager.playerWalkSW.getKeyFrame(player.getStateTime(), true), player.getX(), player.getY());
             }
         }
-        
-      //draw zombies
+
+        //draw zombies
         for (int i = 0; i < world.getZombie().size(); i++) {
-        if(zombie.get(i).isFacingNorth()){
-            //draw the zombie N picture
-            batch.draw(AssetManager.zombieN, zombie.get(i).getX(), zombie.get(i).getY());
-        }//if zombie is facing S
-        else if(zombie.get(i).isFacingSouth()){
-            
-            //draw the zombie S picture
-            batch.draw(AssetManager.zombieS, zombie.get(i).getX(),zombie.get(i).getY());
-        }//if zombie is facing E
-        else if(zombie.get(i).isFacingEast()){
-            //draw the zombie E picture
-            batch.draw(AssetManager.zombieE, zombie.get(i).getX(), zombie.get(i).getY());
-        }//if zombie facing W
-        else if(zombie.get(i).isFacingWest()){
-            //draw the zombie W picture
-            batch.draw(AssetManager.zombieW, zombie.get(i).getX(), zombie.get(i).getY());
-        }//if zombie facing NE
-        else if(zombie.get(i).isFacingNE()){
-            //draw the zombie NE picture
-            batch.draw(AssetManager.zombieNE, zombie.get(i).getX(), zombie.get(i).getY());
-        }//if zombie is facing NW
-        else if(zombie.get(i).isFacingNW()){
-            //draw the zombie NW picture
-            batch.draw(AssetManager.zombieNW, zombie.get(i).getX(), zombie.get(i).getY());
-        }//if zombie is facing SE
-        else if(zombie.get(i).isFacingSE()){
-            //draw the zombie SE picture
-            batch.draw(AssetManager.zombieSE, zombie.get(i).getX(),zombie.get(i).getY());
-        }//if zombie facing SW
-        else if(zombie.get(i).isFacingSW()){
-            //draw the zombie SW picture
-            batch.draw(AssetManager.zombieSW, zombie.get(i).getX(), zombie.get(i).getY());
-        }
+            if (zombie.get(i).isFacingNorth()) {
+                //draw the zombie N picture
+                batch.draw(AssetManager.zombieN, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie is facing S
+            else if (zombie.get(i).isFacingSouth()) {
+
+                //draw the zombie S picture
+                batch.draw(AssetManager.zombieS, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie is facing E
+            else if (zombie.get(i).isFacingEast()) {
+                //draw the zombie E picture
+                batch.draw(AssetManager.zombieE, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie facing W
+            else if (zombie.get(i).isFacingWest()) {
+                //draw the zombie W picture
+                batch.draw(AssetManager.zombieW, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie facing NE
+            else if (zombie.get(i).isFacingNE()) {
+                //draw the zombie NE picture
+                batch.draw(AssetManager.zombieNE, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie is facing NW
+            else if (zombie.get(i).isFacingNW()) {
+                //draw the zombie NW picture
+                batch.draw(AssetManager.zombieNW, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie is facing SE
+            else if (zombie.get(i).isFacingSE()) {
+                //draw the zombie SE picture
+                batch.draw(AssetManager.zombieSE, zombie.get(i).getX(), zombie.get(i).getY());
+            }//if zombie facing SW
+            else if (zombie.get(i).isFacingSW()) {
+                //draw the zombie SW picture
+                batch.draw(AssetManager.zombieSW, zombie.get(i).getX(), zombie.get(i).getY());
+            }
         }
         // finished listing things to draw
         batch.end();
         shapeRenderer.end();
-    
+
     }
+
     public void resize(int width, int height) {
         viewport.update(width, height);
     }
-    
+
     float getMousePosInGameWorldx() {
- Vector3 n = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
- return n.x;
-}
+        Vector3 n = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        return n.x;
+    }
+
     float getMousePosInGameWorldy() {
- Vector3 n = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
- return n.y;
-}
-    
-  
+        Vector3 n = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        return n.y;
+    }
+
 }
