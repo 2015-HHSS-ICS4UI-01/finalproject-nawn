f763d8357f662d561bd9354b7b02268257108228
nitin <nitin@Nitin-PC>
2016-01-25 11:14:03 -0500

    Done Game

--- core/src/com/finalproject/MainGame/MainGame.java
+++ core/src/com/finalproject/MainGame/MainGame.java
@@ -9,6 +9,13 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.finalproject.Model.Bullet;
 import com.finalproject.Model.Cursor;
 import com.finalproject.Model.Player;
@@ -39,7 +46,9 @@ public class MainGame implements Screen {
     private boolean isShoot;
     private int shoot = 0;
     private int clip = 0;
+    private TiledMap map;
     Game g;
+
     public MainGame(Game g) {
         theWorld = new World();
         player = theWorld.getPlayer();
@@ -48,27 +57,26 @@ public class MainGame implements Screen {
         bullet = theWorld.getBullet();
         cursor = theWorld.getCursor();
         isShoot = false;
-    
+
         create();
-		this.g=g;
+        this.g = g;
     }
-    
-    public MainGame(){
+
+    public MainGame() {
         create();
     }
-    
-     public void create(){
-        if(player.getHealth()==0){
-            
-            g.setScreen( new EndScreen());
-        } 
-            if(zombie.size() == 1){
+
+    public void create() {
+        if (player.getHealth() == 0) {
+
+            g.setScreen(new EndScreen());
+        }
+        if (zombie.size() == 1) {
             g.setScreen(new WinScreen());
         }
-    
-}
-    
-    
+
+    }
+
     @Override
     public void show() {
 
@@ -79,57 +87,57 @@ public class MainGame implements Screen {
     public void render(float deltaTime) {
         AssetManager.game.play();
         AssetManager.game.setLooping(true);
-        
-        if(player.getHealth() ==0){
+
+        if (player.getHealth() == 0) {
             create();
             AssetManager.game.setLooping(false);
             AssetManager.game.dispose();
         }
-        if(zombie.size()==1){
+        if (zombie.size() == 1) {
             create();
             AssetManager.game.setLooping(false);
             AssetManager.game.dispose();
         }
-        
+
         //shoot button
-        if(Gdx.input.isButtonPressed(Buttons.LEFT)){
-           
+        if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
+
             isShoot = true;
             for (int i = 0; i < bullet.size(); i++) {
                 AssetManager.gunShot.play();
             }
         }
-        if(clip==9){
-             
+        if (clip == 9) {
+
             clip = 0;
         }
-        
+
         //shooting
-        if(!isShoot){
+        if (!isShoot) {
             for (int i = 0; i < bullet.size(); i++) {
-               bullet.get(i).setX(player.getX());
-               bullet.get(i).setY(player.getY());
+                bullet.get(i).setX(player.getX());
+                bullet.get(i).setY(player.getY());
             }
-        }else{
-           fire(bullet.get(clip));
+        } else {
+            fire(bullet.get(clip));
             for (int i = 1; i < 10; i++) {
-               bullet.get(i).setX(player.getX());
-               bullet.get(i).setY(player.getY());
+                bullet.get(i).setX(player.getX());
+                bullet.get(i).setY(player.getY());
             }
-           shoot++;
+            shoot++;
         }
-        if(shoot==1){
-          cursorfinalx = cursor.getx();
-          cursorfinaly = cursor.gety();
-          playerfinalx = player.getX();
-          playerfinaly = player.getY();         
+        if (shoot == 1) {
+            cursorfinalx = cursor.getx();
+            cursorfinaly = cursor.gety();
+            playerfinalx = player.getX();
+            playerfinaly = player.getY();
         }
         //zombie health
-        for (int i = 0; i < theWorld.getZombie().size()-1; i++) {
-         if(zombie.get(i).getheath()<1){
-             zombie.get(i).Alive(false);
-             zombie.remove(i);
-         }   
+        for (int i = 0; i < theWorld.getZombie().size() - 1; i++) {
+            if (zombie.get(i).getheath() < 1) {
+                zombie.get(i).Alive(false);
+                zombie.remove(i);
+            }
         }
         //movement up down left right with keys
         if (Gdx.input.isKeyPressed(Keys.D)) {
@@ -154,7 +162,45 @@ public class MainGame implements Screen {
         } else if (Gdx.input.isKeyPressed(Keys.A) && Gdx.input.isKeyPressed(Keys.S)) {
             player.setVelocityY(-2f);
             player.setVelocityX(-2f);
-        } 
+        }
+
+        //loads the map  
+        map = new TmxMapLoader().load("zombieMap.tmx");
+        //loads collision layer  
+        MapLayer collisionObjectLayer = map.getLayers().get("collision");
+        //finds all objects in the collision layer  
+        MapObjects objects = collisionObjectLayer.getObjects();
+        //gets all the rectangle objects on the map   
+        for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+
+            //declares all rectangle objects as rectangles   
+            Rectangle rectangle = rectangleObject.getRectangle();
+
+            //if the rectangle objects overlaps the player the speeds is decreased  
+            if (Intersector.overlaps(rectangle, player.getBounds())) {
+
+                //find which side it intersected from and based on that slow the speed or initiate the collison  
+                //if hit wall from bottom up  
+                if (player.getY() < rectangle.y) {
+                    player.setVelocityY(-0.5f);
+
+                    //if hit wall from top down  
+                } else if (player.getY() > rectangle.y) {
+                    player.setVelocityY(0.5f);
+
+                    //if hit wall from left right  
+                }
+
+                if (player.getX() > rectangle.x) {
+                    player.setVelocityX(0.5f);
+
+                    //if hit wall from right left  
+                } else if (player.getX() < rectangle.x) {
+                    player.setVelocityX(-0.5f);
+                }
+            }
+        }
+
 //        //if colliding with left part of screen
         if (player.getX() <= 0) {
             player.setVelocityX(0);
@@ -242,18 +288,18 @@ public class MainGame implements Screen {
                 zombie.get(i).setVelocityY(-0.5f - i / 100);
             }
 
-        //if player is higher than zombie move up
+            //if player is higher than zombie move up
             if (player.getY() > zombie.get(i).getY()) {
                 zombie.get(i).setVelocityY(0.5f + i / 100);
             } else if (player.getVelocityY() < zombie.get(i).getY()) {
-            //move down
+                //move down
                 // zombie.setVelocityY(-0.5f);
             } else {
                 zombie.get(i).setVelocityY(0);
             }
             zombie.get(i).update(deltaTime);
         }
-        
+
         player.update(deltaTime);
 
         collisions();
@@ -267,7 +313,8 @@ public class MainGame implements Screen {
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height
+    ) {
         renderer.resize(width, height);
     }
 
@@ -296,8 +343,8 @@ public class MainGame implements Screen {
         for (int i = 0; i < theWorld.getZombie().size() - 1; i++) {
 
             if (player.isColliding(zombie.get(i))) {
-                if(zombie.get(i).isAlive()){
-                player.setHealth((int) (player.getHealth() - 0.00001));
+                if (zombie.get(i).isAlive()) {
+                    player.setHealth((int) (player.getHealth() - 0.00001));
                 }
                 zombie.get(i).setVelocityX(0);
                 zombie.get(i).setVelocityY(0);
@@ -317,8 +364,8 @@ public class MainGame implements Screen {
                     player.setVelocityY(0);
                     zombie.get(i).setVelocityY(0);
 
-                } else {
-                    // fix the smallest overlap
+                } else // fix the smallest overlap
+                {
                     if (overX < overY) {
 
                         // left of the block
@@ -341,151 +388,165 @@ public class MainGame implements Screen {
                     }
                 }
             }
-            
-            
-            for (int j = 0; j < i ; j++) {
-                
+
+            for (int j = 0; j < i; j++) {
+
                 if (player.isColliding(zombie.get(j))) {
-                    if(zombie.get(j).isAlive()){
-                player.setHealth((int) (player.getHealth() - 0.00001));
+                    if (zombie.get(j).isAlive()) {
+                        player.setHealth((int) (player.getHealth() - 0.00001));
                     }
-                zombie.get(j).setVelocityX(0);
-                zombie.get(j).setVelocityY(0);
-                // get overlapping amount
-                float overX = player.getOverlapX(zombie.get(j));
-                float overY = player.getOverlapY(zombie.get(j));
-
-                //just fixing y if not moving
-                if (player.getVelocityX() == 0 && player.getVelocityY() == 0) {
-                    // player is above the block
-                    if (player.getY() > (zombie.get(j).getY())) {
-                        player.addToPosition(0, overY);
-                    } else if (player.getY() < zombie.get(j).getY()) {
-
-                        player.addToPosition(0, -overY);
-                    }
-                    player.setVelocityY(0);
+                    zombie.get(j).setVelocityX(0);
                     zombie.get(j).setVelocityY(0);
+                    // get overlapping amount
+                    float overX = player.getOverlapX(zombie.get(j));
+                    float overY = player.getOverlapY(zombie.get(j));
 
-                } else {
-                    // fix the smallest overlap
-                    if (overX < overY) {
-
-                        // left of the block
-                        if (player.getX() > (zombie.get(j).getX())) {
-                            player.addToPosition((overX), 0);
-                        } else if (player.getX() < zombie.get(j).getX()) {
-                            player.addToPosition(-overX, 0);
-                        }
-
-                    } else {
-//                        player.setHealth(player.getHealth()-10);
-
-                        // above the block
+                    //just fixing y if not moving
+                    if (player.getVelocityX() == 0 && player.getVelocityY() == 0) {
+                        // player is above the block
                         if (player.getY() > (zombie.get(j).getY())) {
-                            player.addToPosition(0, (overY));
-
+                            player.addToPosition(0, overY);
                         } else if (player.getY() < zombie.get(j).getY()) {
+
                             player.addToPosition(0, -overY);
                         }
                         player.setVelocityY(0);
                         zombie.get(j).setVelocityY(0);
+
+                    } else // fix the smallest overlap
+                    {
+                        if (overX < overY) {
+
+                            // left of the block
+                            if (player.getX() > (zombie.get(j).getX())) {
+                                player.addToPosition((overX), 0);
+                            } else if (player.getX() < zombie.get(j).getX()) {
+                                player.addToPosition(-overX, 0);
+                            }
+
+                        } else {
+//                        player.setHealth(player.getHealth()-10);
+
+                            // above the block
+                            if (player.getY() > (zombie.get(j).getY())) {
+                                player.addToPosition(0, (overY));
+
+                            } else if (player.getY() < zombie.get(j).getY()) {
+                                player.addToPosition(0, -overY);
+                            }
+                            player.setVelocityY(0);
+                            zombie.get(j).setVelocityY(0);
+                        }
                     }
                 }
-            }
-                
-            
-            if (zombie.get(j).isColliding(zombie.get(i))) {
-               
-                zombie.get(i).setVelocityX(0);
-                zombie.get(i).setVelocityY(0);
-                // get overlapping amount
-                float overX = zombie.get(j).getOverlapX(zombie.get(i));
-                float overY = zombie.get(j).getOverlapY(zombie.get(i));
 
-                //just fixing y if not moving
-                if (zombie.get(j).getVelocityX() == 0 && zombie.get(j).getVelocityY() == 0) {
-                    // player is above the block
-                    if (zombie.get(j).getY() > (zombie.get(i).getY())) {
-                        zombie.get(j).addToPosition(0, overY);
-                    } else if (zombie.get(j).getY() < zombie.get(i).getY()) {
+                if (zombie.get(j).isColliding(zombie.get(i))) {
 
-                        zombie.get(j).addToPosition(0, -overY);
-                    }
-                    zombie.get(j).setVelocityY(0);
+                    zombie.get(i).setVelocityX(0);
                     zombie.get(i).setVelocityY(0);
+                    // get overlapping amount
+                    float overX = zombie.get(j).getOverlapX(zombie.get(i));
+                    float overY = zombie.get(j).getOverlapY(zombie.get(i));
 
-                } else {
-                    // fix the smallest overlap
-                    if (overX < overY) {
-                        if (zombie.get(j).getX() > (zombie.get(i).getX())) {
-                            zombie.get(j).addToPosition((overX), 0);
-                        } else if (zombie.get(j).getX() < zombie.get(i).getX()) {
-                            zombie.get(j).addToPosition(-overX, 0);
-                        }
-
-                    } else {
-                        // above the block
+                    //just fixing y if not moving
+                    if (zombie.get(j).getVelocityX() == 0 && zombie.get(j).getVelocityY() == 0) {
+                        // player is above the block
                         if (zombie.get(j).getY() > (zombie.get(i).getY())) {
-                            zombie.get(j).addToPosition(0, (overY));
-
+                            zombie.get(j).addToPosition(0, overY);
                         } else if (zombie.get(j).getY() < zombie.get(i).getY()) {
+
                             zombie.get(j).addToPosition(0, -overY);
                         }
                         zombie.get(j).setVelocityY(0);
                         zombie.get(i).setVelocityY(0);
+
+                    } else // fix the smallest overlap
+                    {
+                        if (overX < overY) {
+                            if (zombie.get(j).getX() > (zombie.get(i).getX())) {
+                                zombie.get(j).addToPosition((overX), 0);
+                            } else if (zombie.get(j).getX() < zombie.get(i).getX()) {
+                                zombie.get(j).addToPosition(-overX, 0);
+                            }
+
+                        } else {
+                            // above the block
+                            if (zombie.get(j).getY() > (zombie.get(i).getY())) {
+                                zombie.get(j).addToPosition(0, (overY));
+
+                            } else if (zombie.get(j).getY() < zombie.get(i).getY()) {
+                                zombie.get(j).addToPosition(0, -overY);
+                            }
+                            zombie.get(j).setVelocityY(0);
+                            zombie.get(i).setVelocityY(0);
+                        }
                     }
                 }
             }
-            }
         }
 
     }
-    
-    
-    public void fire(Bullet bullet){
-        
-    float cursorx;
-    float cursory;
-    float playerx;
-    float playery;
-    int speed = 3;
-    
-    cursorx = cursorfinalx;
-    cursory = cursorfinaly;
-    playerx = playerfinalx;
-    playery = playerfinaly;
 
-    
-    double angle = Math.atan2(cursorx-playerx,cursory-playery );
-    
-    double bulletdx = speed*Math.sin(angle);
-    double bulletdy = speed*Math.cos(angle);
-    
-    bullet.setX((float) (bullet.getx()+bulletdx));
-    bullet.setY((float) (bullet.gety()+bulletdy));
+    public void fire(Bullet bullet) {
+
+        float cursorx;
+        float cursory;
+        float playerx;
+        float playery;
+        int speed = 3;
+
+        cursorx = cursorfinalx;
+        cursory = cursorfinaly;
+        playerx = playerfinalx;
+        playery = playerfinaly;
+
+        double angle = Math.atan2(cursorx - playerx, cursory - playery);
+
+        double bulletdx = speed * Math.sin(angle);
+        double bulletdy = speed * Math.cos(angle);
+
+        bullet.setX((float) (bullet.getx() + bulletdx));
+        bullet.setY((float) (bullet.gety() + bulletdy));
         for (int i = 0; i < theWorld.getZombie().size(); i++) {
-            if(zombie.get(i).isAlive()){
-                
-            if(bullet.isColliding(zombie.get(i))){
-                zombie.get(i).sethealth(zombie.get(i).getheath()-50);
+            if (zombie.get(i).isAlive()) {
+
+                if (bullet.isColliding(zombie.get(i))) {
+                    zombie.get(i).sethealth(zombie.get(i).getheath() - 50);
+                    reset();
+                }
+            }
+        }
+        if (shoot == 100) {
+            reset();
+        }
+        map = new TmxMapLoader().load("zombieMap.tmx");
+        //loads collision layer  
+        MapLayer collisionObjectLayer = map.getLayers().get("collision");
+        //finds all objects in the collision layer  
+        MapObjects objects = collisionObjectLayer.getObjects();
+        //gets all the rectangle objects on the map   
+        for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+
+            //declares all rectangle objects as rectangles   
+            Rectangle rectangle = rectangleObject.getRectangle();
+
+            //if the rectangle objects overlaps the player the speeds is decreased  
+            if (Intersector.overlaps(rectangle, bullet.getBounds())) {
                 reset();
             }
-            }
         }
-    if(shoot == 100){
-        reset();
+
     }
-}
-    public void reset(){
-    isShoot = false;
-    shoot = 0;
-    
-}
-    
-    public boolean shoot(){
-        
-    return isShoot;
+
+    public void reset() {
+        isShoot = false;
+        shoot = 0;
+
+    }
+
+    public boolean shoot() {
+
+        return isShoot;
     }
 
 }
